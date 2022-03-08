98fc95a43eb32712185c5a169f7af2ded0f055c6
Hoangcodezaq <101127326+Hoangcodezaq@users.noreply.github.com>
2022-03-08 17:55:27 +0700

    Add files via upload
    adc

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Các bạn nữ 10C8';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Gửi các bạn nữ';
+const contentCard = 'Chúc các bạn nữ lớp mình luôn xinh đẹp và học giỏi, thay mặt con trai 10C8 tui xin dành những lời đẹp nhất đến cho những bông hoa 10C8 ';
+
+
+const giftImage = 'chucmung.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
